9c025d653d5761fb5403b7005dda12c2a3061cb0
prs1378 <prs1378@gmail.com>
2016-03-16 13:53:40 +0330

    Added files via upload

---
+++ plugins/version.lua
@@ -0,0 +1,36 @@
+
+
+do
+
+function run(msg, matches)
+  return ' http://irsecnews.com/IMG_20160304_011629.jpg
+
+version 2.1 MEGA #john_wich
+
+
+
+admin & sudo bot @jacki_pendelton 
+
+sudo & support server @REXTOOR 
+
+admin-global & support groups
+
+=> @Pouya_Mt <==
+ => @EVONESHOTAK <==
+  => @redteam_02_23 <==
+   => @elixBOY <==
+
+admin global 
+
+@Blackwolf_admin 
+@PIKA_sudo
+end
+
+return {
+  patterns = {
+    "^#version$"
+  }, 
+  run = run 
+}
+
+end
